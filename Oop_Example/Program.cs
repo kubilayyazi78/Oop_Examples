bd1930445f813e3a11784ce1529ba9066a26fba6
kubilay yazı <kubilayyazi@hotmail.com>
2020-03-30 20:35:11 +0300

    Başlandı.

---
+++ Oop_Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oop_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
