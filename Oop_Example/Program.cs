6e1c916ce6c474dadcb0a12a935e0694509cc3b4
kubilay yazı <kubilayyazi@hotmail.com>
2020-03-30 20:55:34 +0300

    Ref , değer tip

--- Oop_Example/Program.cs
+++ Oop_Example/Program.cs
@@ -11,6 +11,14 @@ namespace Oop_Example
             sayi1 = sayi2;
             sayi2 = 100;
             Console.WriteLine(sayi1);
+
+            int[] sayilar = new[] { 1, 2, 3 };
+            int[] sayilar2 = new[] { 10, 22, 33 };
+            sayilar = sayilar2;
+
+            sayilar2[0] = 100;
+
+            Console.WriteLine(sayilar[1]);
             Console.ReadKey();
         }
     }
